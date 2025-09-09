f648cd182d53c1ffc27e7bca7e87ed73c14fc211
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-09-09 13:54:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-OWNER_ID = int(getenv("OWNER_ID", 6211392720))
+OWNER_ID = int(getenv("OWNER_ID", 5962658076))
 OWNER_USERNAME = getenv("OWNER_USERNAME", "AM_ITACHIUCHIHA")
 BOT_USERNAME = getenv("BOT_USERNAME", "iMusichubbot")
 BOT_NAME = getenv("BOT_NAME", "MusicHub Test")
@@ -20,7 +20,7 @@ EVALOP = list(map(int, getenv("EVALOP", "6797202080").split()))
 
 # ───── Mongo & Logging ───── #
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-LOGGER_ID = int(getenv("LOGGER_ID", -1002517323765))
+LOGGER_ID = int(getenv("LOGGER_ID", ))
 
 # ───── Limits and Durations ───── #
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
